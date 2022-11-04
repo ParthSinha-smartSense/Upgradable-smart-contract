64846db85efaf571b4f937035d76a607de3bd992
Parth Sinha <parth.sinha@smartsensesolutions.com>
2022-11-04 13:56:02 +0530

    Renamed UUPS test script and modified TransparentProxy test scripts

--- test/unit/TransparentProxy/01_ProxyTesting.test.js
+++ test/unit/TransparentProxy/01_ProxyTesting.test.js
@@ -1,18 +1,20 @@
 const Proxy = artifacts.require("Proxy");
 const truffleAssert = require("truffle-assertions");
-const Helper = artifacts.require("helper");
 const version1 = artifacts.require("CounterV1");
+const version2 = artifacts.require("CounterV2");
 
 require("web3");
 const chainID = config.network_id;
 
 (chainID == 5777 ? contract : contract.skip)(
-  "Proxy-CounterV1",
+  "Proxy-Counter",
   async function (accounts) {
     before(async () => {
       instance = await Proxy.deployed();
-      help = await Helper.deployed();
       v1 = await version1.deployed();
+      v2 = await version2.deployed();
+      contractInstance1 = await version1.at(instance.address);
+      contractInstance2 = await version2.at(instance.address);
     });
 
     it("Test case - Admin returns correctly", async () => {
@@ -42,13 +44,24 @@ const chainID = config.network_id;
     });
 
     it("Test case - Increment Counter", async () => {
-      let temp = await help.inc.call();
-      await web3.eth.sendTransaction({
-        from: accounts[0],
-        to: instance.address,
-        data: temp,
-      });
-      instance.count.call().then((x) => assert.equal(x, 1, "Increment failed"));
+      await contractInstance1.inc.sendTransaction();
+      contractInstance1.count
+        .call()
+        .then((x) => assert.equal(x, 1, "Increment failed"));
     });
+
+    it("Test case - Update code", async()=>{
+      await instance._setImplementation.sendTransaction(v2.address);
+      await instance._getImplementation.call().then((x) => {
+        assert.equal(x, v2.address, "Wrong address set");
+      });
+    })
+
+    it("Test case - Decrement Counter",async()=>{
+      await contractInstance2.dec.sendTransaction();
+      contractInstance2.count
+        .call()
+        .then((x) => assert.equal(x, 0, "Decrement failed"));
+    })
   }
 );
